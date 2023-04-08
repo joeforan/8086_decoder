e60d3ccb93015b04025ce1272a0b739b6c847024
Joe Foran <joeforan@gmail.com>
2023-04-08 17:07:13 +0900

    Read arg and extract filename

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,3 +1,10 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("Usage: args[0] binary_file");
+        return;
+    }
+    println!("Decoding {}", args[1]);
 }
